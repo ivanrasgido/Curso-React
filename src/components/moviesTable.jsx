abdcc5cd8c9e8938a675d6f6ee55df258252213e
ivanrasgido <ivanrasgido19@gmail.com>
2021-04-09 00:10:49 -0300

    ejercicio de routing aplicado al proyecto de vidly

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,15 +1,17 @@
 import React , { Component } from 'react';
 import Table from './common/table';
 import Like from "./common/like";
+import { Link } from 'react-router-dom';
 
 class MoviesTable extends Component {
   
     columns = [
-        { path: 'title' , label: 'Title'},
+        { path: 'title' , label: 'Title' , content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>},
         { path: 'genre.name' , label: 'Genre'},
         { path: 'numberInStock' , label: 'Stock'},
         { path: 'dailyRentalRate' , label: 'Rate'},
-        { key: 'like' , content : movie => <Like 
+        { key: 'like' , content : movie => 
+        <Like 
         liked={movie.liked} 
         onClick={() => this.props.onLike(movie)} /> },
         { key: 'delete' , content : movie =>(<button 
